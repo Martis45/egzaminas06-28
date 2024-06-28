efcc5f8c8570aad9f0ab32c0d8666660b0f93536
Martis45 <113464473+Martis45@users.noreply.github.com>
2024-06-28 11:52:23 +0300

    Add files via upload

---
+++ egzaminass/index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="stilius.css">
+    <title>Side menu</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row flex-nowrap">
+            <div class="bg-dark col-auto col-md-2 col-lg-2 min-vh-100 d-flex flex-column justify-content-between">
+                <div class="bg-dark p-2">
+                    <a class="d-flex text-decoration-none mt-1 align-items-center text-white">
+                        <span class="fs-4 d-none d-sm-inline">Menu</span>
+                    </a>
+                    <ul class="nav nav-pills flex-column mt-4">
+                        <li class="nav-item py-2 py-sm-0">
+                            <a href="#" class="nav-link text-white" aria-current="page">
+                                <i class="fs-5 fa fa-guage"></i><span class="fs-4 ms-3 d-none d-sm-inline">Home</span>
+                            </a>
+                        </li>
+                        <li class="nav-item py-2 py-sm-0">
+                        <a href="registration.php" class="nav-link text-white" aria-current="page">
+                                <i class="fs-5 fa fa-table-list"></i><span class="fs-4 ms-3 d-none d-sm-inline">Login</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="dropdown open">
+                    <button
+                        class="btn btn-secondary dropdown-toggle"
+                        type="button"
+                        id="triggerId"
+                        data-bs-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                    >
+                        <i class="fa fa-user"></i><span class="ms-2">User</span>
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="triggerId">
+                        <button class="dropdown-item" href="#">Setting</button>
+                        <button class="dropdown-item disabled" href="#">Profile</button>
+                    </div>
+                </div>
+            </div>
+            <div class="p-5">
+                <h2 class="text-center">Riedlentės Pigiau</h2>
+                <h3 class="text-center text-danger">Išpardavimas</h3>
+                <div class="row">
+                <div class="card" style="width: 18rem; margin: 30px;">
+  <img src="https://www.toastiekids.com/cdn/shop/files/BW-SKATEBOARD-NAVY-04_grande.jpg?v=1708447603" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text">RIEDLENTĖ BANWOOD Pajuskite visišką laisvę ir džiaugsmą.</p>
+    <a href="#" class="btn btn-primary">Plačiau</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem; margin: 30px;">
+  <img src="https://toysatfoys.com/wp-content/uploads/2022/05/TY6317-scaled-600x400.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text">Riedlentė OUTLINER 3108–10 Išskirtinė OUTLINER riedlentė pasižymi universalumu.</p>
+    <a href="#" class="btn btn-primary">Plačiau</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem; margin: 30px;">
+  <img src="https://artistskateboard.com/storage/2022/06/Cindy-Sherman-x-Supreme-Skateboard-Deck-Set-of-2-600x400.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text">RIEDLENTĖ STREET SURFING WAVE LX SNAKE PIT.</p>
+    <a href="#" class="btn btn-primary">Plačiau</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem; margin: 30px;">
+  <img src="https://artistskateboard.com/storage/2022/06/Martin-Wong-x-Supreme-Big-Heat-Iglesia-Pentecostal-Skateboard-Deck-Set-600x400.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text">RIEDLENTĖ RAZOR RIPSTIK AIR PRO - tai įranga mėgstantiems išsiskirti.</p>
+    <a href="#" class="btn btn-primary">Plačiau</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem; margin: 30px;">
+  <img src="https://www.mediastorehouse.com.au/p/172/dog-bulldog-skateboard-digital-manipulation-5270135.jpg.webp" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text">Riedlentė Enuff Logo Stain Complete, medžio.</p>
+    <a href="#" class="btn btn-primary">Plačiau</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem; margin: 30px;">
+  <img src="https://www.tomahawkskateboards.com/wp-content/uploads/2020/06/7Y1A9711_web1-600x400.gif" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text">RIEDLENTĖ NILS EXTREME PNB01 pasižymi naujovišku dizainu.</p>
+    <a href="#" class="btn btn-primary">Plačiau</a>
+  </div>
+</div>
+
+</div>
+
+<div class="p-3 col-centered">
+                <h2 class="text-center">Riedlentės</h2>
+                <h3 class="text-center text-danger">Mūsų rekomenduojamos riedlentės jums!</h3>
+                <div class="row">
+                <div class="card" style="width: 18rem; margin: 30px;">
+  <img src="https://toysatfoys.com/wp-content/uploads/2022/10/TY6268-600x400.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text">RIEDLENTĖ KAMAKA plačiai naudojama visame pasaulyje!</p>
+    <a href="#" class="btn btn-primary">Plačiau</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem; margin: 30px;">
+  <img src="https://artistskateboard.com/storage/2022/06/Takashi-Murakami-Flaming-Skull-Rainbow-Skateboard-Deck-Set-600x400.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text">Riedlentė ARTIST labiausiai perkama riedlentė mūsų el.parduotuvėje.</p>
+    <a href="#" class="btn btn-primary">Plačiau</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem; margin: 30px;">
+  <img src="https://www.shutterstock.com/image-photo/two-neon-rainbow-colored-penny-600nw-2315069391.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text">RIEDLENTĖ RAINBOW, riedlenė kurią pamils kiekvienas vaikas.</p>
+    <a href="#" class="btn btn-primary">Plačiau</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem; margin: 30px;">
+  <img src="https://x.cloudsdata.net/kg/images/products/originals/TR51705-000-234737-.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text">RIEDLENTĖ FANTASTIC, nebežinau ką rašyti apie šią riedlentę. </p>
+    <a href="#" class="btn btn-primary">Plačiau</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem; margin: 30px;">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTumbgLynXHQUFUDRtx700YJDLznBegDQsezg&s" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text">El.Riedlentė GTR Bamboo, skirta ir miesto ir užmieščio keliams.</p>
+    <a href="#" class="btn btn-primary">Plačiau</a>
+  </div>
+</div>
+
+</div>
+
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
